9e71e620d14b20508939bc9fe3bbfe3c42dfeed5
MclPio <113801014+MclPio@users.noreply.github.com>
2023-03-25 15:57:10 -0400

    edit build_tree method

--- lib/bst.rb
+++ lib/bst.rb
@@ -21,14 +21,14 @@ class Tree
     @array = array.uniq.sort
   end
 
-  def build_tree(array = @array, start_idx = 0, end_idx = array.length - 1)
+  def build_tree(array = self.array, start_idx = 0, end_idx = array.length - 1)
     return nil if start_idx > end_idx
 
     mid = (start_idx + end_idx) / 2
     root = Node.new(array[mid])
     root.left = build_tree(array, start_idx, mid - 1)
     root.right = build_tree(array, mid + 1, end_idx)
-    root
+    self.root = root
   end
 
   def pretty_print(node = root, prefix = '', is_left = true)
@@ -36,16 +36,18 @@ class Tree
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
+
+  def insert
+  end
+
+  def delete
+  end
 end
 
 
 # Tests
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-a = Node.new(arr)
-b = Node.new(arr.map { |ele| ele+2})
-a < b
 tree = Tree.new(arr)
-root = tree.build_tree
-p root
-tree.root = root
+tree.build_tree
 tree.pretty_print
+p tree
